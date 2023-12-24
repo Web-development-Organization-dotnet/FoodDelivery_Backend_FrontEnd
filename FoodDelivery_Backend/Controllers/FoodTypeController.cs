474f3c79f92d7422ab2bdc718583826c2a53deef
AgnivDas <dasagniv10@gmail.com>
2023-12-24 12:02:39 +0530

    Food Type update
    Food Type API update.

---
+++ FoodDelivery_Backend/Controllers/FoodTypeController.cs
@@ -0,0 +1,42 @@
+﻿using FoodDelivery_Backend.Data;
+using FoodDelivery_Backend.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using ActionNameAttribute = System.Web.Http.ActionNameAttribute;
+using HttpPostAttribute = System.Web.Http.HttpPostAttribute;
+
+namespace FoodDelivery_Backend.Controllers
+{
+    public class FoodTypeController : Controller
+    {
+        Food_Delivery_DbEntities db_obj = new Food_Delivery_DbEntities();
+
+
+
+        // GET: FoodType
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        [ActionName("insertFoodType")]
+        public String insertFoodType([FromBody] FoodTypeModel val)
+        {
+            var model = new tbl_food_type
+            {
+                
+            };
+
+            db_obj.tbl_food_type.Add(model);
+            db_obj.SaveChanges();
+
+            return "Food Type Successfully Registered!!";
+        }
+    }
+}
---
+++ FoodDelivery_Backend/Models/FoodTypeModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodDelivery_Backend.Models
+{
+    public class FoodTypeModel
+    {
+        public string food_type_cd { get; set; }
+        public string food_type { get; set; }
+        public string type_desc { get; set; }
+        public string food_category { get; set; }
+    }
+}
