9044dd381a202dd81360bb679962d636cf773a90
Subham <shubham.kundu97@gmail.com>
2023-12-30 19:49:08 +0530

    Food Type Registration

--- FoodDelivery_Backend/Controllers/FoodTypeController.cs
+++ FoodDelivery_Backend/Controllers/FoodTypeController.cs
@@ -2,7 +2,9 @@
 using FoodDelivery_Backend.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 
@@ -24,17 +26,32 @@ namespace FoodDelivery_Backend.Controllers
 
         [HttpPost]
         [ActionName("RegisterFoodType")]
-        public String insertFoodType([FromBody] FoodTypeModel val)
+        public async Task<String>  insertFoodType([FromBody] FoodTypeModel val)
         {
-            var model = new tbl_food_type
+            string cd = val.food_type.Substring(0, 2) + "-" + val.food_category.Substring(0, 2);
+            cd = cd.ToUpper();
+            var query = await db_obj.tbl_food_type.Where( a=> a.food_type_cd == cd).FirstOrDefaultAsync();
+            if (query == null)
             {
-                
-            };
+                var model = new tbl_food_type()
+                {
+                    food_category = val.food_category,
+                    food_type = val.food_type,
+                    food_type_cd = cd,
+                    type_desc = val.type_desc
 
-            db_obj.tbl_food_type.Add(model);
-            db_obj.SaveChanges();
+                };
 
-            return "Food Type Successfully Registered!!";
+
+                db_obj.tbl_food_type.Add(model);
+                db_obj.SaveChanges();
+
+                return "Food Type Successfully Registered!!";
+            }
+            else 
+            {
+                return "Food Type Not Registered!!";
+            }
         }
     }
 }
