5f789278f0ec909b571211f1885c28c7fa70f649
Indranil Chatterjee <indranilc98@gmail.com>
2024-01-04 17:40:57 +0530

    Exception code added

--- FoodDelivery_Backend/Controllers/FoodTypeController.cs
+++ FoodDelivery_Backend/Controllers/FoodTypeController.cs
@@ -1,9 +1,12 @@
 ﻿using FoodDelivery_Backend.Data;
 using FoodDelivery_Backend.Models;
+using FoodDelivery_Backend.Models.Common;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Dynamic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
@@ -13,7 +16,7 @@ namespace FoodDelivery_Backend.Controllers
     public class FoodTypeController : ApiController
     {
         Food_Delivery_DbEntities db_obj = new Food_Delivery_DbEntities();
-        
+
         [HttpGet]
         [ActionName("FoodTypeDetails")]
         [Route("FoodTypeDetails/{Id}")]
@@ -26,32 +29,56 @@ namespace FoodDelivery_Backend.Controllers
 
         [HttpPost]
         [ActionName("RegisterFoodType")]
-        public async Task<String>  insertFoodType([FromBody] FoodTypeModel val)
+        public async Task<IHttpActionResult> InsertFoodType([FromBody] FoodTypeModel val)
         {
-            string cd = val.food_type.Substring(0, 2) + "-" + val.food_category.Substring(0, 2);
-            cd = cd.ToUpper();
-            var query = await db_obj.tbl_food_type.Where( a=> a.food_type_cd == cd).FirstOrDefaultAsync();
-            if (query == null)
+            try
             {
-                var model = new tbl_food_type()
+                if (val.food_type.Length > 1 && val.food_category.Length > 1)
                 {
-                    food_category = val.food_category,
-                    food_type = val.food_type,
-                    food_type_cd = cd,
-                    type_desc = val.type_desc
+                    // Condition for Creating Food Type Primary Key is 
+                    // First two character of Food Type & '-' & First two charcater of Food Category 
 
-                };
+                    string cd = (val.food_type.Substring(0, 2) + "-" + val.food_category.Substring(0, 2)).ToUpper();
+                    var query = await db_obj.tbl_food_type.Where(a => a.food_type_cd == cd).FirstOrDefaultAsync();
+                    if (query == null)
+                    {
+                        var model = new tbl_food_type()
+                        {
+                            food_category = val.food_category,
+                            food_type = val.food_type,
+                            food_type_cd = cd,
+                            type_desc = val.type_desc
 
+                        };
+                        db_obj.tbl_food_type.Add(model);
+                        db_obj.SaveChanges();
 
-                db_obj.tbl_food_type.Add(model);
-                db_obj.SaveChanges();
+                        return Ok("Food Type Successfully Registered!!");
+                    }
+                    else
+                    {
+                        return BadRequest("Food Type Not Registered!!");
+                    }
+                }
+                else
+                {
+                    return BadRequest("Condition does not satisfied to create unique Queue for Food Type");
 
-                return "Food Type Successfully Registered!!";
+                }
             }
-            else 
+            catch (Exception e)
             {
-                return "Food Type Not Registered!!";
+                return Content(HttpStatusCode.BadRequest, new ErrorResponse()
+                {
+                    stackTrace = e.StackTrace,
+                    originalExceptionMessage = e.Message,
+                    message = "Exception Occured",
+                    innerException = e.InnerException.ToString()
+                });
+
             }
+
+
         }
     }
 }
---
+++ FoodDelivery_Backend/Models/Common/ErrorResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FoodDelivery_Backend.Models.Common
+{
+    public class ErrorResponse
+    {
+        public string stackTrace { get; set; }
+        public string originalExceptionMessage { get; set; }
+        public string message { get; set; }
+        public string innerException { get; set; }
+    }
+}
