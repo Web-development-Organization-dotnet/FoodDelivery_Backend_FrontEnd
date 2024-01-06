678acdbb931ec47f927266e91a6b4088eaf64880
Indranil Chatterjee <indranilc98@gmail.com>
2024-01-06 16:55:44 +0530

    Database Backend Project Added
    Database backend Project Added

---
+++ Food_Delivery_DatabaseProject/dbo/Tables/tbl_agent_category.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[tbl_agent_category] (
+    [agent_type_cd]    VARCHAR (50)   NOT NULL,
+    [type_description] VARCHAR (MAX)  NULL,
+    [comm_pct]         NUMERIC (5, 2) NULL,
+    CONSTRAINT [PK_tbl_agent_category] PRIMARY KEY CLUSTERED ([agent_type_cd] ASC)
+);
+
---
+++ Food_Delivery_DatabaseProject/dbo/Tables/tbl_issue_info.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[tbl_issue_info] (
+    [dispute_no]  NUMERIC (18)  NOT NULL,
+    [raised_on]   DATETIME      NULL,
+    [issue_desc]  VARCHAR (MAX) NULL,
+    [ticket_stat] VARCHAR (MAX) NULL,
+    [resolution]  VARCHAR (MAX) NULL,
+    [resolved_on] DATETIME      NULL,
+    [trans_id]    NUMERIC (18)  NULL,
+    [order_id]    NUMERIC (18)  NULL,
+    CONSTRAINT [PK_tbl_issue_info] PRIMARY KEY CLUSTERED ([dispute_no] ASC),
+    CONSTRAINT [FK_tbl_issue_info_tbl_order_info] FOREIGN KEY ([order_id]) REFERENCES [dbo].[tbl_order_info] ([order_id]),
+    CONSTRAINT [FK_tbl_issue_info_tbl_transaction_info] FOREIGN KEY ([trans_id]) REFERENCES [dbo].[tbl_transaction_info] ([trans_id])
+);
+
