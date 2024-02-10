16bd9d4d8b13a4e5cc974281f64ea02b87261562
AgnivDas <dasagniv10@gmail.com>
2024-02-10 19:36:24 +0530

    Supplier Update and select
    Code changes on 10th Feb 2024

--- FoodDelivery_Backend/Controllers/FoodSupplierController.cs
+++ FoodDelivery_Backend/Controllers/FoodSupplierController.cs
@@ -22,27 +22,27 @@ namespace FoodDelivery_Backend.Controllers
         {
             try
             {
-                    var query = await db_obj.tbl_supplier_type.Where(a => a.supplier_type ==val.supplier_type).FirstOrDefaultAsync();
-                    if (query == null)
+                var query = await db_obj.tbl_supplier_type.Where(a => a.supplier_type == val.supplier_type).FirstOrDefaultAsync();
+                if (query == null)
+                {
+                    var model = new tbl_supplier_type()
                     {
-                        var model = new tbl_supplier_type()
-                        {
-                            supplier_type=val.supplier_type.ToUpper(),
-                            description=val.description,
-                            yearly_turnover=val.yearly_turnover
+                        supplier_type = val.supplier_type.ToUpper(),
+                        description = val.description,
+                        yearly_turnover = val.yearly_turnover
 
-                        };
-                        db_obj.tbl_supplier_type.Add(model);
-                        db_obj.SaveChanges();
+                    };
+                    db_obj.tbl_supplier_type.Add(model);
+                    db_obj.SaveChanges();
 
-                        return Ok("Supplier Type Successfully Registered!!");
-                    }
-                    else
-                    {
-                        return BadRequest("Supplier Type Not Registered!!");
-                    }
-                
-               
+                    return Ok("Supplier Type Successfully Registered!!");
+                }
+                else
+                {
+                    return BadRequest("Supplier Type Not Registered!!");
+                }
+
+
             }
             catch (Exception e)
             {
@@ -59,5 +59,82 @@ namespace FoodDelivery_Backend.Controllers
 
         }
 
+        [HttpPut]
+        [ActionName("UpdateSupplierType")]
+
+        public async Task<IHttpActionResult> UpdateSupplierType([FromBody] SupplierType val)
+        {
+            try
+            {
+                var query = await db_obj.tbl_supplier_type.Where(a => a.supplier_type == val.supplier_type).FirstOrDefaultAsync();
+                if (query != null)
+                {
+                    query.description = val.description;
+                    query.yearly_turnover = val.yearly_turnover;
+
+                    db_obj.SaveChanges();
+
+                    return Ok("Supplier Type Successfully Updated!!");
+                }
+                else
+                {
+                    return BadRequest("Supplier Type Not Updated!!");
+                }
+            }
+            catch (Exception e)
+            {
+                return Content(HttpStatusCode.BadRequest, new ErrorResponse()
+                {
+                    stackTrace = e.StackTrace,
+                    originalExceptionMessage = e.Message,
+                    message = "Exception Occured",
+                    innerException = e.InnerException.ToString()
+                });
+
+            }
+
+        }
+
+        [HttpGet]
+        [ActionName("GetAllSupplierType")]
+
+        public async Task<IHttpActionResult> GetAllSupplierType()
+        {
+            try
+            {
+                var query = await db_obj.tbl_supplier_type.ToListAsync();
+                if (query != null)
+                {
+                    var resultModel = new List<SupplierType>();
+                    foreach (var item in query)
+                    {
+                        var subModel = new SupplierType()
+                        {
+                            supplier_type = item.supplier_type,
+                            description = item.description,
+                            yearly_turnover = item.yearly_turnover ?? 0
+                        };
+                        resultModel.Add(subModel);
+                    }
+                    return Ok(resultModel);
+                }
+                else
+                {
+                    return BadRequest("Supplier Type not Found!!");
+                }
+            }
+            catch (Exception e)
+            {
+                return Content(HttpStatusCode.BadRequest, new ErrorResponse()
+                {
+                    stackTrace = e.StackTrace,
+                    originalExceptionMessage = e.Message,
+                    message = "Exception Occured",
+                    innerException = e.InnerException.ToString()
+                });
+
+            }
+
+        }
     }
 }
