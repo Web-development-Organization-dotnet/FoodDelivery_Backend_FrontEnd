ad0eb92358e856f0dc106644155dff697dd3f242
Subham <shubham.kundu97@gmail.com>
2024-04-20 20:12:44 +0530

    Added model and class

--- FoodDelivery_Angular_Frontend/Frontend-App/src/app/app.component.ts
+++ FoodDelivery_Angular_Frontend/Frontend-App/src/app/app.component.ts
@@ -3,12 +3,14 @@ import { RouterOutlet } from '@angular/router';
 import { BasicFormComponent } from './basic-form/basic-form.component';
 import { RegisterFormComponent } from './register-form/register-form.component';
 import { HelpcontactComponent } from './helpcontact/helpcontact.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
+    FormsModule,
     BasicFormComponent,
     RegisterFormComponent,
     HelpcontactComponent
--- FoodDelivery_Angular_Frontend/Frontend-App/src/app/register-form/register-form.component.ts
+++ FoodDelivery_Angular_Frontend/Frontend-App/src/app/register-form/register-form.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, viewChild } from '@angular/core';
+import { user_registration } from '../Model/registration';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register-form.component.html',
   styleUrl: './register-form.component.css'
 })
 export class RegisterFormComponent {
+_userregistration:user_registration;
+@ViewChild('RegistrationForm')
+RegistrationForm !: NgForm;
+constructor(){
+  this._userregistration=new user_registration();
+}
+
+ngOnInit():void{
+
+}
+onSubmit(){
+  console.log(this._userregistration);
+}
 
 }
