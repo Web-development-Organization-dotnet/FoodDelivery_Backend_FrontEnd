6848a643c70276735bd4f53fd9d1db0c1d5c1a80
AgnivDas <dasagniv10@gmail.com>
2024-08-03 19:31:40 +0530

    code changes on 03/08/2024
    code changes on 03/08/2024 for registration initiate.

---
+++ FoodDelivery_Angular_Frontend/Frontend-App/src/app/Models/register.ts
@@ -0,0 +1,19 @@
+export class registerModel {
+    username: string;
+    email: string;
+    status: string;
+    reg_date: string;
+    phone: string;
+    photo_id_no: string;
+    password: string;
+  
+    constructor() {
+      this.username = '';
+      this.email= '';
+      this.status= '';
+      this.reg_date= '';
+      this.phone= '';
+      this.photo_id_no= '';
+      this.password = ''
+    }
+  }
