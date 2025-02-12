5662ffdce180fdd93c6d1dd0f938abee24db2997
Indranil Chatterjee <indranilc98@gmail.com>
2025-02-12 17:09:14 +0530

    Code pushed
    Code pushed

--- FoodDelivery_Angular_Frontend/Frontend-App/src/app/app.component.ts
+++ FoodDelivery_Angular_Frontend/Frontend-App/src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    RouterOutlet,  
-  ],
+    CommonModule,
+    RouterOutlet,
+    NgxSpinnerModule
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'TEST-CRUD';
-}
+  isLoading =false;
+  title = 'TEST-CRUD'; 
+ }
+
