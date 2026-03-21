a989948785a19fa9187459d197e5a33ae8a803f9
Subham <shubham.kundu97@gmail.com>
2026-03-21 19:28:50 +0530

    Menu-Sidebar

--- FoodDelivery_Angular_Frontend/Frontend-App/src/app/_layout/sidebar/sidebar.component.ts
+++ FoodDelivery_Angular_Frontend/Frontend-App/src/app/_layout/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import menuData from '../../StaticData/menu.json';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
@@ -8,14 +9,15 @@ import menuData from '../../StaticData/menu.json';
   imports: [
     RouterLink,
     RouterLinkActive,
+    CommonModule
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
    // Example role (can come from login/session/API)
-  currentRole: string = 'Supplier'; // Change this to "Admin", "Agent", "Customer", or "Supplier" to test different views
-
+  currentRole: string = ""; // Change this to "Admin", "Agent", "Customer", or "Supplier" to test different views
+  
   // Full menu from JSON
   allMenus: any[] = menuData;
 
@@ -23,8 +25,11 @@ export class SidebarComponent {
   filteredMenus: any[] = [];
 
   ngOnInit(): void {
+    const user = this.getUserData();
+    this.currentRole = user?.role;
     this.filteredMenus = this.filterMenuByRole(this.allMenus);
     console.log('Filtered Menus for Role:', this.currentRole, this.filteredMenus);
+    
   }
 
   filterMenuByRole(menus: any[]): any[] {
@@ -37,4 +42,13 @@ export class SidebarComponent {
         };
       });
   }
+  getUserData(): any | null {
+  try {
+    const data = localStorage.getItem('userDetails');
+    return data ? JSON.parse(data) as any : null;
+  } catch (error) {
+    console.error('Error parsing localStorage data', error);
+    return null;
+  }
+}
 }
