0c904c11deb053cce9ba326257bab4f2801a0971
Subham <shubham.kundu97@gmail.com>
2026-08-01 18:12:27 +0530

    Supplier wallet update

---
+++ FoodDelivery_Angular_Frontend/Frontend-App/src/app/Models/supplierWallet.ts
@@ -0,0 +1,17 @@
+import { supplierInfoModel } from "./supplierInfo";
+
+export class supplierWalletModel {
+    supplier_wallet_id: number;
+    supplier_id: number;
+    wallet_balance: number;
+    SI:supplierInfoModel;
+    last_update_date: string;
+    
+    constructor() {
+        this.supplier_wallet_id = 0;
+        this.supplier_id = 0;
+        this.wallet_balance=0;
+        this.SI=new supplierInfoModel();
+        this.last_update_date = '';
+    }
+}
--- FoodDelivery_Angular_Frontend/Frontend-App/src/app/Pages/Admin/Wallet/supplier-wallet/supplier-wallet.component.ts
+++ FoodDelivery_Angular_Frontend/Frontend-App/src/app/Pages/Admin/Wallet/supplier-wallet/supplier-wallet.component.ts
@@ -5,7 +5,9 @@ import { SidebarComponent } from '../../../../_layout/sidebar/sidebar.component'
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { DataTablesModule } from 'angular-datatables';
-import { RouterLink, RouterLinkActive } from '@angular/router';
+import { ActivatedRoute, Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { SupplierService } from '../../../../Service/Supplier/supplier.service';
+import { supplierWalletModel } from '../../../../Models/supplierWallet';
 
 @Component({
   selector: 'app-supplier-wallet',
@@ -24,5 +26,75 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './supplier-wallet.component.css'
 })
 export class SupplierWalletComponent {
+
+  supplierWalletModelObj:supplierWalletModel=new supplierWalletModel();
+    supplierTypeList:supplierWalletModel[]=[];
+    id:any;
+    constructor (private router:Router, private SupplierServ:SupplierService, private route:ActivatedRoute)
+    {
+      
+    }
+    ngOnInit(){
+  
+      //Get Supplier Type For DropDown
+      this.SupplierServ.getSupplierTypes().subscribe(q=>{
+        console.log(q);      
+        this.supplierTypeList=q;
+      });
+  
+      this.id=this.route.snapshot.paramMap.get('id')?.toString();
+      
+      console.log(this.id);
+      if (this.id) 
+      {
+        this.SupplierServ.getSupplierInfobyId(this.id).subscribe(q=>{
+        this.supplierWalletModelObj = q;
+        this.supplierWalletModelObj.supplier_type=this.supplierWalletModelObj.SI.supplier_name;
+        });
+      }
+    }
+     onSubmit(form: NgForm) {
+        console.log(this.supplierWalletModelObj);
+    
+        if (!form.invalid) {
+          if(!this.id){
+            this.supplierWalletModelObj.SI.supplier_name=this.supplierWalletModelObj.supplier_name;
+            // API Call
+            this.SupplierServ.registerSupplierInfo(this.supplierWalletModelObj).subscribe(q => {
+              console.log('Updateed response', q);
+      
+              if (q && q.message === 'Supplier Info Successfully Registered!!') {
+                alert('Supplier Info Successfully registered!!');
+                this.router.navigate(['/supplierInfoList']);
+              }
+              else {
+                alert('Supplier Info Failed to register!!');
+              }
+            })
+          }        
+          else{
+            this.supplierWalletModelObj.SI.supplier_name=this.supplierWalletModelObj.supplier_name;
+            // API Call
+            this.SupplierServ.UpdateSupplierWallet(this.supplierWalletModelObj).subscribe(q => {
+              console.log('Updateed response', q);
+      
+              if (q && q.message === 'Amount added to your Supplier Wallet successfully!!') {
+                alert('Amount added to your Supplier Wallet successfully!!');
+                this.router.navigate(['/supplierInfoList']);
+              }
+              else {
+                alert('Failed to add amount to your Supplier Wallet!!');
+              }
+            })
+  
+          }
+        }
+        else {
+          // Navigate
+          console.log('Error');
+          //this.router.navigate(['/login']);
+        }
+    
+      }
 
 }
